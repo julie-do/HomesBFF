2c4c0ae2a9c0dad9bfb121c806fee15c12a5a89c
Jelli <julie.nhudo101@gmail.com>
2023-09-04 18:49:35 -0700

    [HomesBFF] Initialize project structure with gitignore.

---
+++ Homes.EntryPoint/Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homes.EntryPoint.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/[controller]")]
+    public class HomeListingsController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<HomeListingsController> _logger;
+
+        public HomeListingsController(ILogger<HomeListingsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
